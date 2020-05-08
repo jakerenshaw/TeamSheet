5f34b21e097005f9e92fd630e3b575c744481efd
Jake Renshaw <jake.renshaw@aol.com>
2020-05-08 16:24:21 +0100

    created rootviewcontroller

---
+++ TeamSheet/RootViewController.swift
@@ -0,0 +1,16 @@
+//
+//  RootViewController.swift
+//  TeamSheet
+//
+//  Created by Jake Renshaw on 08/05/2020.
+//  Copyright © 2020 Jake Renshaw. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
